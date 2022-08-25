056ff654a6e0f4137ec89148132b90875406eee4
Gabriel Meirelles <gabrielsmeirelles@hotmail.com>
2022-08-25 16:17:08 -0300

    placing pieces on the board

---
+++ src/application/chess/pieces/King.java
@@ -0,0 +1,17 @@
+package application.chess.pieces;
+
+import application.boardgame.Board;
+import application.chess.ChessPiece;
+import application.chess.Color;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
---
+++ src/application/chess/pieces/Rook.java
@@ -0,0 +1,17 @@
+package application.chess.pieces;
+
+import application.boardgame.Board;
+import application.chess.ChessPiece;
+import application.chess.Color;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
